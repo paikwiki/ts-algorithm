958b1172140ca4eccc52bb9119d210492f046b3b
paikwiki <paikwiki@gmail.com>
2019-03-19 11:20:01 +0900

    Done q1.1.2: factorial(recursive)

---
+++ src/q1.1.2-factorial-recursive.ts
@@ -0,0 +1,11 @@
+{
+  // Q. n!을 계산하여 반환하는 재귀함수를 작성하라.
+  // A.
+  const factorial2 = (n: number): number => {
+    if (n === 1) return 1;
+    else return n * factorial2(n-1);
+  }
+  // App
+  console.log(factorial2(4)); // 24
+  console.log(factorial2(6)); // 720
+}
