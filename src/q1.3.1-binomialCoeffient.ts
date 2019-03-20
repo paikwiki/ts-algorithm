a54dd141163dfe122bb3b6e5022308cbf12ef668
paikwiki <paikwiki@gmail.com>
2019-03-20 22:23:54 +0900

    Add quiz text on q1.3.1

--- src/q1.3.1-binomialCoeffient.ts
+++ src/q1.3.1-binomialCoeffient.ts
@@ -1,26 +1,32 @@
 import * as readline from 'readline';
+{
+  // Q.
+  // 이항계수 구하기
+  // 점화식을 이용하여 이항계수 계산하기
 
-const choose = (n, r): number => {
-  if( r === 0 || n === r) return 1;
+  // A.
+  const choose = (n, r): number => {
+    if( r === 0 || n === r) return 1;
 
-  return choose(n-1, r-1) + choose(n-1, r);
+    return choose(n-1, r-1) + choose(n-1, r);
+  }
+
+  const io = (): void => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    rl.question('input n, r: ', (answer: string) => {
+      rl.close();
+      let n: number;
+      let r: number;
+
+      [n, r] = answer.split(' ').map(i=>parseInt(i));
+      
+      console.log(choose(n, r));
+    })
+  }
+
+  io();
 }
-
-const io = (): void => {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-  rl.question('input n, r: ', (answer: string) => {
-    rl.close();
-    let n: number;
-    let r: number;
-
-    [n, r] = answer.split(' ').map(i=>parseInt(i));
-    
-    console.log(choose(n, r));
-  })
-}
-
-io();
