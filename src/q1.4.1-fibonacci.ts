ee41aa8b6d82b94d4667fb61cbe1c0a34fbaa3af
paikwiki <paikwiki@gmail.com>
2019-03-22 11:53:31 +0900

    Add return type of function and comment value of result

--- src/q1.4.1-fibonacci.ts
+++ src/q1.4.1-fibonacci.ts
@@ -3,7 +3,7 @@
   // 피보나치 수열의 n번째 항 Fn을 계산하여 반환하는 함수를 작성하고, f20, f50을 구하라.
 
   // A.
-  const fibo = (n: number) => {
+  const fibo = (n: number): number => {
     if(n === 1 || n === 2) return 1;
     else return fibo(n-1) + fibo(n-2);
   }
